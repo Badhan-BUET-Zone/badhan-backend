5e032a20e113637bb32e125584f9d751e3a40473
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2021-02-04 17:25:05 +0600

    :sparkles: fixed app.js and mongoose.js stuff

--- app.js
+++ app.js
@@ -1,75 +1,102 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require('dotenv')
+//LOAD ENV VARS
+dotenv.config( { path : './config/config.env'} )
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const env = process.env.NODE_ENV || 'development';
 
-var app = express();
+console.log(env);
 
-
-//CORS Security
-const securityEnabled = true;
-
-if (securityEnabled) {
-    app.use(function (req, res, next) {
-
-        // Website you wish to allow to connect
-        //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        if (req.headers.origin) {
-            res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        }
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        // Pass to next layer of middleware
-        next();
-    });
-}
+// if (env === 'development') {
+//     process.env.MONGODB_URI = 'mongodb+srv://shwarup:shawroop@product.xwfvv.mongodb.net/Chaldal?retryWrites=true&w=majority';
+// } else {
+//     process.env.MONGODB_URI = 'mongodb+srv://shwarup:shawroop@product.xwfvv.mongodb.net/Chaldal?retryWrites=true&w=majority';
+// }
+const exec = require('child_process').exec
+const fs = require('fs')
+const colors = require('colors')
+const errorHandler = require('./middleware/error')
+const history = require('connect-history-api-fallback');
+const bodyParser=require('body-parser');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let cors = require('cors');
 
 
 
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+let apiRouter = require('./routes/api');
+
+const { mongoose } = require('./db/mongoose');
+
+// //Load Models 
+// const Product = require('./db/models/product').Product
+// //Emptying the database
+// Product.collection.deleteMany()
+// console.log("Data deleted".red.inverse);
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+let app = express();
+
+//SPA Handling
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+app.use( errorHandler )
+
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    // res.render('error');
 });
+
+process.on('SIGINT', async function () {
+    
+    //todo shift from console.error to something more...reasonable
+    console.error('SIGINT called');
+    await mongoose.disconnect();
+    console.error('Mongoose connection terminated');
+    process.exit(0);
+});
+
+process.on('SIGTERM', async function () {
+    console.error('SIGTERM called');
+    await mongoose.disconnect();
+    console.error('Mongoose connection terminated');
+    process.exit(0);
+});
+
 
 module.exports = app;
