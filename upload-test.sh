315abf7103ef5588e2a8201cb5c7963fc53fa64b
Mahathir <mirmahathir1@gmail.com>
2021-07-15 12:33:31 +0600

    Added test upload script

---
+++ upload-test.sh
@@ -0,0 +1,9 @@
+echo "Enter commit message: "
+read -r message
+echo "The commit message is: $message"
+node doc/swagger.js
+git add .
+git commit -am "$message"
+
+git push origin test-branch
+git push heroku-test test-branch:master
