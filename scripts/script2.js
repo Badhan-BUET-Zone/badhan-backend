73cda11f5151727696300ab641a357c3a5ccd36e
Mahathir <mirmahathir1@gmail.com>
2021-08-03 14:16:57 +0600

    fixed bug for blood group A+ in search route

--- scripts/script2.js
+++ scripts/script2.js
@@ -7,15 +7,30 @@ const {Donation} = require('../db/models/Donation');
 const util = require('util')
 
 const test = async()=>{
-    let donors = await Donor.find({studentId:{$type:16}}).limit(5);
+    let donors = await Donor.aggregate([
+        {$group: {
+                _id: {name: "$phone"},
+                uniqueIds: {$addToSet: "$_id"},
+                count: {$sum: 1}
+            }
+        },
+        {$match: {
+                count: {"$gt": 1}
+            }
+        },
+        {$sort: {
+                count: -1
+            }
+        }
+    ]);
     console.log(donors.length)
-    for(let i = 0 ; i < donors.length; i++){
+    // for(let i = 0 ; i < donors.length; i++){
         // console.log(typeof donors[i].studentId);
         // donors[i].studentId = donors[i].studentId.toString();
 
         // console.log(donors[i].studentId);
         // await donors[i].save();
-    }
+    // }
     await mongoose.disconnect();
 }
 test();
