86f4a1a95d17f5a3bc423db94a38b11c63d36095
Mahathir <mirmahathir1@gmail.com>
2021-09-22 01:29:30 +0600

    Added ip address to userAgent middleware

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ const {userAgentHandler} = require('./middlewares/userAgent');
 
 app.use(cors());
 
-
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- middlewares/userAgent.js
+++ middlewares/userAgent.js
@@ -1,6 +1,12 @@
 const useragent = require('useragent');
 const userAgentHandler = (req, res, next) => {
-    req.userAgent = useragent.parse(req.headers['user-agent']).toString();
+    let agent = useragent.parse(req.headers['user-agent'])
+    req.userAgent={
+        os:agent.os.toString(),
+        device: agent.device.toString(),
+        ip: req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress
+    }
+    console.log("useragent os->",req.userAgent.os," device->",req.userAgent.device," ip->",req.userAgent.ip);
     next();
 }
 module.exports={
