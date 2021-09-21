0b1f840ff6e7177427d93fc3b78e0e8e36d8f3f6
Mahathir <mirmahathir1@gmail.com>
2021-09-22 01:37:54 +0600

    added browser family in userAgent middleware

--- middlewares/userAgent.js
+++ middlewares/userAgent.js
@@ -1,12 +1,13 @@
 const useragent = require('useragent');
 const userAgentHandler = (req, res, next) => {
-    let agent = useragent.parse(req.headers['user-agent'])
+    let agent = useragent.parse(req.headers['user-agent']).
     req.userAgent={
         os:agent.os.toString(),
         device: agent.device.toString(),
+        family: agent.family.toString(),
         ip: req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress
     }
-    console.log("useragent os->",req.userAgent.os," device->",req.userAgent.device," ip->",req.userAgent.ip);
+    console.log("useragent os->",req.userAgent.os," device->",req.userAgent.device," family->",req.userAgent.family," ip->",req.userAgent.ip);
     next();
 }
 module.exports={
