43a17093bac95f04408b4de89441ec578e70657c
Mahathir <mirmahathir1@gmail.com>
2021-10-29 22:45:30 +0600

    added environment variable to enable/disable ratelimiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,69 +1,70 @@
 const rateLimit = require("express-rate-limit");
 const {TooManyRequestsError429}= require('../response/errorTypes');
 
-const devDisable = process.env.NODE_ENV === 'development'?1000:1;
+const rateLimiterEnabled = process.env.RATE_LIMITER_ENABLE === 'true'?1:1000;
 const minute = 60*1000;
+
 const commonRateLimiterError = new TooManyRequestsError429("Service unavailable");
 
 const signInLimiter = rateLimit({
     windowMs: 5 * minute,
-    max: 3*devDisable,
+    max: 3*rateLimiterEnabled,
     message:new TooManyRequestsError429("Please try again after 5 minutes")
 });
 
 const redirectionSignInLimiter = rateLimit({
     windowMs: minute,
-    max: 3*devDisable,
+    max: 3*rateLimiterEnabled,
     message:commonRateLimiterError
 });
 
 const donationInsertionLimiter = rateLimit({
-    windowMs: 10*minute,
-    max: 240*devDisable,
+    windowMs: minute,
+    max: 12*rateLimiterEnabled,
     message:commonRateLimiterError
 });
 const donorInsertionLimiter = rateLimit({
-    windowMs: 10*minute,
-    max: 240*devDisable,
+    windowMs: minute,
+    max: 12*rateLimiterEnabled,
     message:commonRateLimiterError
 });
 const donorDeletionLimiter = rateLimit({
-    windowMs: 10*minute,
-    max: 240*devDisable,
+    windowMs: minute,
+    max: 12*rateLimiterEnabled,
     message:commonRateLimiterError
 });
 const deleteDonationLimiter = rateLimit({
-    windowMs: 10*minute,
-    max: 240*devDisable,
+    windowMs: minute,
+    max: 12*rateLimiterEnabled,
     message: commonRateLimiterError
 });
 
 const commonLimiter = rateLimit({
-    windowMs: 10*minute,
-    max: 240*devDisable,
+    windowMs: minute,
+    max: 12*rateLimiterEnabled,
     message: commonRateLimiterError
 });
 
 const passwordRequestLimiter = rateLimit({
     windowMs: minute,
-    max: 3*devDisable,
+    max: 3*rateLimiterEnabled,
     message: commonRateLimiterError
 });
 
 const passwordForgotLimiter = rateLimit({
     windowMs: 3 * minute,
-    max: 1*devDisable,
+    max: 1,
     message: new TooManyRequestsError429("Please try again after 3 minutes")
 });
 
 const publicContactInsertionLimiter = rateLimit({
     windowMs: minute,
-    max: 12*devDisable,
+    max: 12*rateLimiterEnabled,
     message: commonRateLimiterError
 })
 const publicContactDeletionLimiter = rateLimit({
     windowMs: minute,
-    max: 12*devDisable,
+    max: 12*rateLimiterEnabled,
     message: commonRateLimiterError
 })
 
