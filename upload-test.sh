de0694d8664f8ecd3af36fa27694fc5ab7148186
Mir Mahathir Mohammad <mirmahathir1@gmail.com>
2021-11-15 14:55:00 +0600

    added swagger output to git

--- upload-test.sh
+++ upload-test.sh
@@ -2,8 +2,7 @@ git checkout test-branch
 echo "Enter commit message: "
 read -r message
 echo "The commit message is: $message"
-node run doc
+npm run doc
 git add .
 git commit -am "$message"
-
 git push origin test-branch
