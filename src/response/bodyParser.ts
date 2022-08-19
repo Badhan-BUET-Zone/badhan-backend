80c9f8b6bd0d35e8de8156913115095aeab1556c
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-08-19 19:26:48 +0600

    refactor bodyParser.ts

--- src/response/bodyParser.ts
+++ src/response/bodyParser.ts
@@ -1,9 +1,10 @@
-// @ts-nocheck
-/* tslint:disable */
+import { Response, Request, NextFunction } from 'express'
 import { BadRequestError400 } from './errorTypes'
-export function handleJsonBodyParseFailures (err, request, response, next) {
-  if (err instanceof SyntaxError && err.status === 400) {
-    return response.status(400).send(new BadRequestError400('Malformed JSON'))
+import { HttpException} from "./models/HttpException";
+
+export function handleJsonBodyParseFailures (err: HttpException, request: Request, response: Response, next: NextFunction) {
+  if (err.status === 400) {
+    return response.status(400).send(new BadRequestError400('Malformed JSON',{}))
   }
   next()
 }
--- upload.sh
+++ upload.sh
@@ -4,16 +4,6 @@ git checkout master
 echo "---merge test branch---"
 git merge test-branch
 
-echo "Enter commit message: "
-read -r message
-echo "The commit message is: $message"
-
-echo "---adding changed files to staging---"
-git add .
-
-echo "---committing staged files---"
-git commit -am "$message"
-
 # push to mahathir backend repo
 echo "---pushing master branch to github---"
 git push origin master
