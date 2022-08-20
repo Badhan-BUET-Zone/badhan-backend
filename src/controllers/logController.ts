7e3faa4bd6b1f89141763a411f76df77e40d1003
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-08-21 01:23:09 +0600

    logController to typescript

--- src/controllers/logController.ts
+++ src/controllers/logController.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-/* tslint:disable */
 import * as donorInterface from '../db/interfaces/donorInterface'
 import * as donationInterface from '../db/interfaces/donationInterface'
 import * as logInterface from '../db/interfaces/logInterface'
@@ -9,8 +7,8 @@ import ForbiddenError403 from "../response/models/errorTypes/ForbiddenError403";
 import InternalServerError500 from "../response/models/errorTypes/InternalServerError500";
 import { handleGETGitReleaseInfo } from '../microservices/githubAPI'
 import {handleGETFirebaseGooglePlayVersion} from '../microservices/firebaseAPI'
-
-const validate = require('jsonschema').validate
+import {validate} from 'jsonschema'
+import {Response, Request} from 'express'
 
 const githubResponseSchema = {
   type: 'object',
@@ -44,47 +42,47 @@ const firebaseResponseSchema = {
   required: ['backendBaseURL', 'backendTestBaseURL', 'version']
 }
 
-function isValidHttpUrl (string) {
+function isValidHttpUrl (text: string) {
   let url
   try {
-    url = new URL(string)
+    url = new URL(text)
   } catch (_) {
     return false
   }
   return url.protocol === 'http:' || url.protocol === 'https:'
 }
 
-function isValidVersion (string) {
-  return /^\d{1,2}\.\d{1,2}\.\d{1,2}$/.test(string)
+function isValidVersion (text: string) {
+  return /^\d{1,2}\.\d{1,2}\.\d{1,2}$/.test(text)
 }
 
-const handleGETAppVersions = async (req, res) => {
+const handleGETAppVersions = async (req: Request, res: Response) => {
   const githubResponse = await handleGETGitReleaseInfo()
 
   const githubValidationResult = validate(githubResponse.data, githubResponseSchema)
   if (githubValidationResult.errors.length !== 0) {
-    return res.status(500).send(new InternalServerError500('Github API response format not valid'))
+    return res.status(500).send(new InternalServerError500('Github API response format not valid',{},{}))
   }
   const browserDownloadURL = githubResponse.data.assets[0].browser_download_url
   const githubReleaseVersion = githubResponse.data.tag_name
 
   if (!isValidHttpUrl(browserDownloadURL) || !isValidVersion(githubReleaseVersion)) {
-    return res.status(500).send(new InternalServerError500('Github release browser download URL/ version is not valid'))
+    return res.status(500).send(new InternalServerError500('Github release browser download URL/ version is not valid',{},{}))
   }
 
   const firebaseResponse = await handleGETFirebaseGooglePlayVersion()
   const firebaseValidationResult = validate(firebaseResponse.data, firebaseResponseSchema)
   if (firebaseValidationResult.errors.length !== 0 || !isValidVersion(firebaseResponse.data.version)) {
-    return res.status(500).send(new InternalServerError500('Firebase frontendSettings has invalid format'))
+    return res.status(500).send(new InternalServerError500('Firebase frontendSettings has invalid format',{},{}))
   }
   return res.status(200).send(new OKResponse200('Github and firebase latest app version fetched', {
-    githubReleaseVersion: githubReleaseVersion,
+    githubReleaseVersion,
     githubReleaseDownloadURL: browserDownloadURL,
     firebaseVersion: firebaseResponse.data.version
   }))
 }
 
-const handleGETStatistics = async (req, res) => {
+const handleGETStatistics = async (req: Request, res: Response) => {
   const donorCount = await donorInterface.getCount()
   const donationCount = await donationInterface.getCount()
   const volunteerCount = await donorInterface.getVolunteerCount()
@@ -97,33 +95,33 @@ const handleGETStatistics = async (req, res) => {
   }))
 }
 
-const handleGETLogs = async (req, res) => {
+const handleGETLogs = async (req: Request, res: Response) => {
   const logCountsResult = await logInterface.getLogCounts()
   return res.status(200).send(new OKResponse200('Log counts fetched successfully', {
     logs: logCountsResult.data
   }))
 }
 
-const handleGETLogsByDate = async (req, res) => {
-  const logsByDateResult = await logInterface.getLogsByDate(req.params.date)
+const handleGETLogsByDate = async (req: Request<{date: string},{},{},{}>, res: Response) => {
+  const logsByDateResult = await logInterface.getLogsByDate(parseInt(req.params.date,10))
   return res.status(200).send(new OKResponse200('Logs fetched by date successfully', {
     logs: logsByDateResult.data
   }))
 }
-const handleGETLogsByDateAndDonor = async (req, res) => {
-  const logsByDateAndDonor = await logInterface.getLogsByDateAndUser(req.params.date, req.params.donorId)
+const handleGETLogsByDateAndDonor = async (req: Request, res: Response) => {
+  const logsByDateAndDonor = await logInterface.getLogsByDateAndUser(parseInt(req.params.date,10), req.params.donorId)
   return res.status(200).send(new OKResponse200('Logs fetched by data and donor successfully', {
     logs: logsByDateAndDonor.data
   }))
 }
 
-const handleDELETELogs = async (req, res) => {
+const handleDELETELogs = async (req: Request, res: Response) => {
   if (!res.locals.middlewareResponse.donor._id.equals(MASTER_ADMIN_ID)) {
-    return res.status(403).send(new ForbiddenError403('Only Master Admin is allowed to access this route'))
+    return res.status(403).send(new ForbiddenError403('Only Master Admin is allowed to access this route',{}))
   }
   await logInterface.deleteLogs()
   await logInterface.addLogOfMasterAdmin('DELETE LOGS', {})
-  return res.status(200).send(new OKResponse200('All logs deleted successfully'))
+  return res.status(200).send(new OKResponse200('All logs deleted successfully',{}))
 }
 
 export default {
