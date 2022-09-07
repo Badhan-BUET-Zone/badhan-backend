41ac0b2f62b16e04693b2c702b3ba52d20d4301f
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2022-09-07 12:46:17 +0600

    removed reading gmail auth in dotenv

--- src/dotenv/index.ts
+++ src/dotenv/index.ts
@@ -6,10 +6,10 @@ dotenv.config({ path: '.env.' + process.env.NODE_ENV })
 interface DotenvEnvFile {
   NODE_ENV: string,
   JWT_SECRET: string,
-  GMAIL_CLIENT_ID: string,
-  GMAIL_CLIENT_SECRET: string,
-  GMAIL_REDIRECT_URI: string,
-  GMAIL_REFRESH_TOKEN: string,
+  // GMAIL_CLIENT_ID: string,
+  // GMAIL_CLIENT_SECRET: string,
+  // GMAIL_REDIRECT_URI: string,
+  // GMAIL_REFRESH_TOKEN: string,
   VUE_APP_FRONTEND_BASE: string,
   RATE_LIMITER_ENABLE: string,
   MONGODB_URI: string
@@ -18,10 +18,10 @@ interface DotenvEnvFile {
 const dotenvEnvFile: DotenvEnvFile = {
   NODE_ENV: process.env.NODE_ENV!,
   JWT_SECRET: process.env.JWT_SECRET!,
-  GMAIL_CLIENT_ID: process.env.GMAIL_CLIENT_ID!,
-  GMAIL_CLIENT_SECRET: process.env.GMAIL_CLIENT_SECRET!,
-  GMAIL_REDIRECT_URI: process.env.GMAIL_REDIRECT_URI!,
-  GMAIL_REFRESH_TOKEN: process.env.GMAIL_REFRESH_TOKEN!,
+  // GMAIL_CLIENT_ID: process.env.GMAIL_CLIENT_ID!,
+  // GMAIL_CLIENT_SECRET: process.env.GMAIL_CLIENT_SECRET!,
+  // GMAIL_REDIRECT_URI: process.env.GMAIL_REDIRECT_URI!,
+  // GMAIL_REFRESH_TOKEN: process.env.GMAIL_REFRESH_TOKEN!,
   VUE_APP_FRONTEND_BASE: process.env.VUE_APP_FRONTEND_BASE!,
   RATE_LIMITER_ENABLE: process.env.RATE_LIMITER_ENABLE!,
   MONGODB_URI: process.env.MONGODB_URI!
